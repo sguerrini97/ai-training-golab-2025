9e12057f0597ceb7b0ff96a0979faf93bc1b6876
Florin Pățan <florinpatan@gmail.com>
2025-10-05 10:54:31 +0200

    Ex 06 - chunking

--- cmd/examples/example06/main.go
+++ cmd/examples/example06/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
+	"github.com/ardanlabs/ai-training/foundation/client"
 	"github.com/ardanlabs/ai-training/foundation/mongodb"
 )
 
@@ -76,6 +80,78 @@ func run() error {
 	}
 
 	fmt.Println("\nYou can now use example07 to ask questions about this content.")
+
+	return nil
+}
+
+func createBookEmbeddings(ctx context.Context) error {
+	llm := client.NewLLM(url, model)
+
+	if _, err := os.Stat("zarf/data/book.embeddings"); err == nil {
+		return nil
+	}
+
+	data, err := os.ReadFile("zarf/data/book.chunks")
+	if err != nil {
+		return fmt.Errorf("read file: %w", err)
+	}
+
+	output, err := os.Create("zarf/data/book.embeddings")
+	if err != nil {
+		return fmt.Errorf("create file: %w", err)
+	}
+	defer output.Close()
+
+	fmt.Print("\n")
+	fmt.Print("\033[s")
+
+	r := regexp.MustCompile(`<CHUNK>[\w\W]*?<\/CHUNK>`)
+	chunks := r.FindAllString(string(data), -1)
+
+	// Read one chunk at a time (each line) and get the vector embedding.
+	for counter, chunk := range chunks {
+		fmt.Print("\033[u\033[K")
+		fmt.Printf("Vectorizing Data: %d of %d", counter, len(chunks))
+
+		chunk := strings.Trim(chunk, "<CHUNK>")
+		chunk = strings.Trim(chunk, "</CHUNK>")
+
+		// YOU WILL WANT TO KNOW HOW MANY TOKENS ARE CURRENTLY IN THE CHUNK
+		// SO YOU DON'T EXCEED THE NUMBER OF TOKENS THE MODEL WILL USE TO
+		// CREATE THE VECTOR EMBEDDING. THE MODEL WILL TRUNCATE YOUR CHUNK IF IT
+		// EXCEEDS THE NUMBER OF TOKENS IT CAN USE TO CREATE THE VECTOR
+		// EMBEDDING. THERE ARE MODELS THAT ONLY VECTORIZE AS LITTLE AS 512
+		// TOKENS. THERE IS A TIKTOKEN PACKAGE IN FOUNDATION TO HELP YOU WITH
+		// THIS.
+
+		vector, err := llm.EmbedText(ctx, chunk)
+		if err != nil {
+			return fmt.Errorf("embedding: %w", err)
+		}
+
+		doc := document{
+			ID:        counter,
+			Text:      chunk,
+			Embedding: vector,
+		}
+
+		data, err := json.Marshal(doc)
+		if err != nil {
+			return fmt.Errorf("marshal: %w", err)
+		}
+
+		// Write the json document to the embeddings file.
+		if _, err := output.Write(data); err != nil {
+			return fmt.Errorf("write: %w", err)
+		}
+
+		// Write a crlf for easier read access.
+		if _, err := output.Write([]byte{'\n'}); err != nil {
+			return fmt.Errorf("write crlf: %w", err)
+		}
+	}
+
+	fmt.Print("\n")
 
 	return nil
 }
